fb3c7f485891091384bc6998835144d6105cf1c9
talhaansari4761 <talhaansari4761@gmail.com>
2021-12-22 21:57:53 +0530

    deploy

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json()) 
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return(
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>    
+                </Scroll>    
+            </div>
+        );
+    }
+}
+
+export default App;
